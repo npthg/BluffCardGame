711ed054c22c05ab5374767a0580e8a50efb804b
Napat Thongpao <npt.thongpao@gmail.com>
2019-03-27 15:01:28 +0700

    start screen , show random picture card , show point , show player name

--- MainScreen.py
+++ MainScreen.py
@@ -4,7 +4,6 @@ from models import Game
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 500
 
-
 class MainScreenWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -12,12 +11,25 @@ class MainScreenWindow(arcade.Window):
 
         self.game = Game(width, height)
 
+        self.card = self.game.card
+        self.cardSprite = arcade.Sprite(self.card.cardImage())
+        self.cardSprite.center_x = self.card.x
+        self.cardSprite.center_y = self.card.y
+
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_text(self.game.startButton.name, self.game.startButton.x, self.game.startButton.y, arcade.color.WHITE)
+        if(self.game.startButton.pressed):
+            self.cardSprite.draw()
+            arcade.draw_text(self.game.player_one_label.name, self.game.player_one_label.x, self.game.player_one_label.y,
+                             arcade.color.WHITE)
+            arcade.draw_text(self.game.next_label.name, self.game.next_label.x, self.game.next_label.y,
+                             arcade.color.WHITE)
+            arcade.draw_text('Point: '+str(self.game.player_one.point), 320, 480,
+                             arcade.color.WHITE)
 
-        arcade.draw_text(self.game.startButton, (self.width/2)-10, self.height/2, arcade.color.WHITE)
-
-
+    def on_mouse_press(self, x, y, button, modifiers):
+        self.game.on_mouse_press(button, modifiers)
 
 if __name__== '__main__':
     window = MainScreenWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,8 +1,80 @@
+import arcade
+from random import randint
+
+PLAYER_ROUND = 0
+
+class Player:
+    def __init__(self, game, playername):
+        self.game = game
+        self.playerName = playername
+
+        self.point = 100
+
+    #def updatePoint(self):
+    #    self.point+=10
+
+class Card:
+    def __init__(self, game, x, y):
+        self.game = game
+        self.x = x
+        self.y = y
+        self.cardType = randint(0, 3)
+
+        self.imageList = ['images/dog.png', 'images/cat.png', 'images/pig.png', 'images/rat.png']
+
+    def cardImage(self):
+        image = ''
+        if(self.cardType == 0):
+            image = self.imageList[0]
+        elif(self.cardType == 1):
+            image = self.imageList[1]
+        elif(self.cardType == 2):
+            image = self.imageList[2]
+        elif(self.cardType == 3):
+            image = self.imageList[3]
+
+        return image
+
+    def get_CardType(self):
+        return self.cardType
+
+    def card_disappear(self):
+        self.x = self.game.width
+        self.y = self.game.height
+
+class Label:
+    def __init__(self, game, x, y, name):
+        self.game = game
+        self.x = x
+        self.y = y
+        self.name = name
+        self.pressed = False
+
+    def label_disappear(self):
+        self.x = self.game.width
+        self.y = self.game.height
+
+    def on_press(self):
+        self.pressed = True
+    def on_release(self):
+        self.pressed = False
 
 class Game:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-        self.startButton = 'Start'
+        self.startButton = Label(self, 200, 250, 'Start')
+        self.player_one_label = Label(self, 10, 480, 'Player1')
+        self.player_two_label = Label(self, 10, 480, 'Player2')
+        self.next_label = Label(self, 360, 20, 'Next')
 
+        self.card = Card(self, 200, 250)
+
+        self.player_one = Player(self, 1)
+        self.player_two = Player(self, 2)
+
+    def on_mouse_press(self, button, modifiers):
+        if(button == arcade.MOUSE_BUTTON_LEFT):
+            self.startButton.label_disappear()
+            self.startButton.pressed = True
